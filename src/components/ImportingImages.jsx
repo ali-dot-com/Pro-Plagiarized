617f1050073064f89342c4ed2d743803ae87ab5b
ali-dot-com <aliali444735@gmail.com>
2023-02-18 20:40:27 +0500

    Complete landing page except samples

--- src/components/ImportingImages.jsx
+++ src/components/ImportingImages.jsx
@@ -1,10 +1,11 @@
 import i1 from '../assets/24-7.png'
 import i2 from '../assets/timing.png'
+import i3 from '../assets/man.png'
 import i4 from '../assets/checked.png'
 import i5 from '../assets/badge.png'
 import i6 from '../assets/writer.png'
 import i7 from '../assets/test.png' 
 
 export {
-    i1,i2,i4,i5,i6,i7
+    i1,i2,i3,i4,i5,i6,i7
 };
